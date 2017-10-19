f923e45abe6ca8bb0643e37bcaee52c24e700cbb
Tarek Ziade <tarek@ziade.org>
2017-10-19 15:06:37 +0200

    boilerplate from foule.es

---
+++ content/test.rst
@@ -0,0 +1,11 @@
+Youpi
+#####
+
+:date: 2017-10-17
+:category: actu
+:author: Tarek Ziade
+
+Mamoune regarde, le site fonctionne.
+
+Je travaillons dessus!
+
---
+++ pelicanconf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Tarek Ziadé'
+SITENAME = "Les Cartables de l'Oze"
+SITEURL = ''    #http://foule.es'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+MENUITEMS = [('Accueil', '/')]
+STATIC_PAGES = {}
+THEME = "themes/tarek"
+
+DATE_FORMATS = {'fr': ('fr_FR.UTF-8','%a %d %b %Y'),
+                }
+#DISQUS_SITENAME = "foulees"
+
+ARTICLE_SAVE_AS = ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_LANG_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}-{lang}.html'
+PAGE_SAVE_AS = PAGE_URL = '{slug}.html'
+PAGE_LANG_URL = '{slug}-{lang}.html'
+
+PDF_GENERATOR = False
+FEED_RSS = 'feed'
+TAG_FEED_RSS  = 'tag/%s/feed'
+LOCALE = 'fr_FR.UTF-8'
